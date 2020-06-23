6b2f54d26d47dda59185f6a441b6f7dd96009042
Zernach <ryan@zernach.com>
2020-06-23 00:41:14 -0400

    adds in_order_print

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -71,16 +71,19 @@ class BSTNode:
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
+    #def in_order_print(self, node):
+    #    if node is None:
+    #        pass
+    #    self.in_order_print(node.left)
+    #    print(node.value)
+    #    self.in_order_print(node.right)
+
     def in_order_print(self, node):
-        if node:
-            if node.left:
-                node.in_order_print(node.left)
-
-
-            print(node.value)
-
-            if node.right:
-                node.in_order_print(node.right)
+        if node.left:
+            node.in_order_print(node.left)
+        print(node.value)
+        if node.right:
+            node.in_order_print(node.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
