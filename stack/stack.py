30df519041c3a35c9f453ee27d954df9184fc605
Zernach <ryan@zernach.com>
2020-06-23 21:04:26 -0400

    final touches to stack data structure code

--- stack/stack.py
+++ stack/stack.py
@@ -5,7 +5,6 @@ from singly_linked_list import LinkedList
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
-
 1. Implement the Stack class using an array as the underlying storage structure.
    Make sure the Stack tests pass.
 2. Re-implement the Stack class, this time using the linked list implementation
